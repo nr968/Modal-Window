c76e98456c5704cb802318ca75e818fed3f5ce17
nr968 <nrnaveenkmr@gmail.com>
2022-07-19 22:35:27 +0530

    Click events completed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+"use strict";
 
+const buttonsEl = document.querySelectorAll(".btn");
+console.log(buttonsEl);
+const modelEl = document.querySelector(".model-window");
+const overlayEl = document.querySelector(".overlay");
+
+const closeEl = document.querySelector(".close-btn");
+
+function hiddenToggle() {
+  modelEl.classList.toggle("hidden");
+  overlayEl.classList.toggle("hidden");
+}
+
+for (let i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", hiddenToggle);
+}
+closeEl.addEventListener("click", hiddenToggle);
+overlayEl.addEventListener("click", hiddenToggle);
